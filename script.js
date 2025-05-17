49b502cb8db882638f6a51baf275b0fa20875133
Faheembukshprog <fbux12@gmail.com>
2025-05-17 18:02:28 +0500

    "Initial commit"

---
+++ script.js
@@ -0,0 +1,11 @@
+function showForm(formId) {
+    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
+    document.getElementById(formId).classList.add("active");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const activeForm = document.querySelector(".form-box.active");
+    if (!activeForm) {
+        showForm('login-form');
+    }
+});
